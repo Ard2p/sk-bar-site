a62a4eceae226ef3509a4ae855c3a4f250bfa825
Ard2p <Ard2p@bk.ru>
2024-04-18 21:13:35 +0300

    галлерея фикс

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -1,6 +1,6 @@
 @props(['items'])
 
-<div class="row row-cols-lg-6 row-cols-md-3 row-cols-1 g-1 gallery">
+<div class="row row-cols-lg-6 row-cols-3 g-1 gallery">
     @foreach ($items as $item)
         <div class="col">
             <div class="ratio ratio-1x1">
