f62470963a8c705e522e68b88adbbbd0f8057db9
Ard2p <Ard2p@bk.ru>
2024-07-25 05:58:52 +0300

    резерв промежуточный результат

--- app/Http/Controllers/ReservController.php
+++ app/Http/Controllers/ReservController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\Reserv;
+use SergiX44\Nutgram\Nutgram;
 use App\Enums\ReservStatusEnum;
 use App\Enums\ReservTablesEnum;
 use App\Http\Requests\ReservReservRequest;
-use App\Models\Event;
 
 class ReservController extends Controller
 {
@@ -34,12 +35,25 @@ class ReservController extends Controller
         return $reservs;
     }
 
-    public function reserv(ReservReservRequest $request)
+    public function reserv(ReservReservRequest $request, Nutgram $bot)
     {
         $reserv = Reserv::create([
             ...$request->validated(),
             'status' => ReservStatusEnum::PENDING->value
         ]);
+
+        $bot->sendMessage(
+            $request->validated()['table'],
+            config('nutgram.reserv_group'),
+            parse_mode: 'HTML'
+        );
+
+        // $bot->sendMessage((string)view('telegram.admin.message.details', [
+        //     'id' => $bot->user()->id,
+        //     'firstName' => $bot->user()->first_name,
+        //     'lastName' => $bot->user()->last_name,
+        //     'username' => $bot->user()->username ?? $bot->user()->first_name
+        // ]), config('nutgram.admin_group'), parse_mode: 'HTML');
 
         return response('ok');
     }
