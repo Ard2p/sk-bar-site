6e62e10a19f1c3b47572d66e60d29c6d41a2d187
Ard2p <ard2p@bk.ru>
2024-12-20 16:17:43 +0300

    Статус резерва после бронирования RESERV

--- app/Http/Controllers/ReservController.php
+++ app/Http/Controllers/ReservController.php
@@ -43,7 +43,7 @@ class ReservController extends Controller
 
             $reserv = Reserv::create([
                 ...$request->validated(),
-                'status' => ReservStatusEnum::PENDING->value
+                'status' => ReservStatusEnum::RESERV->value
             ]);
 
             ReservHandler::reserv($bot, $reserv);
